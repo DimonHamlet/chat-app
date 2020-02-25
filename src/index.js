5d7d7a2001e7ff2d0e2ca19840c7eb75f07d16c4
Dima <formyblogger69@gmail.com>
2020-02-25 22:29:42 +0200

    Adding Socket IO package(and simple configuration) to project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const path = require('path')
+const http = require('http')
+const socketio = require('socket.io')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 const port = process.env.PORT || 3000
 
@@ -9,7 +13,10 @@ const publicDirPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirPath))
 
+io.on('connection', () => {
+    console.log('New web-socket connection')
+})
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log('Server is up on port ', port)
 })
