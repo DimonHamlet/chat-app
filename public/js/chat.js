cae034987068fabe85d04363ec9eaa011079b9c6
Dima <formyblogger69@gmail.com>
2020-02-26 21:54:21 +0200

    Basic functionality is done and project is closed

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,39 @@
-io()
+const socket = io()
+
+socket.on('sayHello', (hello) => {
+    console.log(hello)
+})
+// Elements 
+const $messageForm = document.getElementById('ChatForm')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+
+$messageForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    //disable
+    $messageFormButton.setAttribute('disabled', 'disabled')
+
+    const message = $messageFormInput.value
+
+    socket.emit('sendMessage', message, (status) => {
+        // enable
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+        console.log(status)
+    })
+})
+
+socket.on('viewMessage', (msg) => {
+    document.getElementById('msgField').innerText += msg + '\n'
+})
+
+document.getElementById('shareId').addEventListener('click', (e) => {
+    if (!navigator.geolocation) {
+        return alert('Your browser doesnt support Geolocation')
+    }
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('shareLocation', position.coords.latitude, position.coords.longitude,)
+    })
+})
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const http = require('http')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
 const server = http.createServer(app)
@@ -13,8 +14,28 @@ const publicDirPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirPath))
 
-io.on('connection', () => {
-    console.log('New web-socket connection')
+
+io.on('connection', (socket) => {
+    socket.emit('sayHello', 'Welcom!')
+    socket.broadcast.emit('sayHello', 'A new user joined')
+    
+    socket.on('sendMessage', (message, callback) => {
+        const filter = new Filter()
+        if (filter.isProfane(message)) {
+            return callback('Profanity is not allowed')
+        }
+        
+        io.emit('viewMessage', message)
+        callback('Messegae was delivered')
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('sayHello', 'User has left')
+    })
+
+    socket.on('shareLocation', (lat, lon, callback) => {
+        socket.broadcast.emit('sayHello', `https://google.com/maps?q=${lat},${lon}`)
+    })
 })
 
 server.listen(port, () => {
